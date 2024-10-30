23f2ab8cacfb06b5dc4087c861adb890590b19ed
eliasuc <elias.usaqui@utec.edu.pe>
2024-10-29 23:25:34 -0500

    subiendo primer archivo

---
+++ inicio_code.py
@@ -0,0 +1,6 @@
+#Este es el primer avance del proyecto de TIC TAC TOE
+#Deberia de conseguir un buen nombre y creativo nombre para este proyecto
+#Primero importariamos las librerias correspondientes
+import random
+
+#al parecer tambien usaremos multiples funciones
